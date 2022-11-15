55476b9c2051e81c18122a8a681a7e8b3824d2e2
cihatozkan <cihat.ozkan91@gmail.com>
2022-11-15 21:25:50 +0300

    SingleArraySum.py

---
+++ SingleArraySum.py
@@ -0,0 +1,14 @@
+"""
+HackerRank Single Array Sum
+https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+"""
+
+def singleArraySum(ar):
+    return sum(ar)   #sum func returns sum of all integer elements of array.
+
+singleArraySum(X)
+
+
+
+
