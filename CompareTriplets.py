6a33b8b58de692cf70bd0e8b3bc81d4337f20473
cihatozkan <cihat.ozkan91@gmail.com>
2022-11-15 21:46:07 +0300

    CompareTriplets.py

---
+++ CompareTriplets.py
@@ -0,0 +1,19 @@
+"""
+HackerRank Single Array Sum
+https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+"""
+
+def compareTriplets(a, b):
+    sumAlice = 0
+    sumBob = 0
+    for i in range(len(a)): # We could get len of b also. Because arrays(a,b) always have same lenght.
+        print(i)
+        if a[i]>b[i]:
+            sumAlice=sumAlice+1
+        elif a[i]<b[i]:
+            sumBob=sumBob+1
+        else:
+            continue
+    return sumAlice,sumBob
+
+compareTriplets(x,y)
